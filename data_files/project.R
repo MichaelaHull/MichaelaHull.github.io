4cf350ec5c0e788ae39d1176a148f9c0101360d5
ferrocene <mmhull@dons.usfca.edu>
2015-05-07 16:46:08 -0700

    Links, more prototype
    Getting more graphs on the prototype, updating links for the prototype,
    updating data files.

--- data_files/project.R
+++ data_files/project.R
@@ -46,6 +46,7 @@ other <- mutate(other, gen_cat = "other")
 data <- rbind(theft, controlled, assault, kidnapping, conduct, other)
 
 ggplot() + geom_histogram(data = data, aes(x = PdDistrict))
+ggplot() + geom_histogram(data = data, aes(x = gen_cat))
 
 data <- mutate(data, Category = properCase(Category))
 data <- mutate(data, Descript = properCase(Descript))
@@ -62,7 +63,7 @@ write.csv(data, "sfpd_incidents_2014.csv", row.names = FALSE)
 
 # Manually creating a time series
 ts_data <- mutate(data, theft = 0)
-ts_data <- mutate(ts_data, conduct = 0)
+ts_data <- mutate(ts_data, vandalism = 0)
 ts_data <- mutate(ts_data, kidnapping = 0)
 ts_data <- mutate(ts_data, controlled = 0)
 ts_data <- mutate(ts_data, assault = 0)
@@ -70,8 +71,8 @@ ts_data <- mutate(ts_data, other = 0)
 
 theft <- filter(ts_data, gen_cat == "Theft")
 theft$theft = 1
-conduct <- filter(ts_data, gen_cat == "Conduct")
-conduct$conduct = 1
+vandalism <- filter(ts_data, gen_cat == "Vandalism")
+vandalism$vandalism = 1
 kidnapping <- filter(ts_data, gen_cat == "Kidnapping")
 kidnapping$kidnapping = 1
 controlled <- filter(ts_data, gen_cat == "Controlled")
@@ -80,9 +81,51 @@ assault <- filter(ts_data, gen_cat == "Assault")
 assault$assault = 1
 other <- filter(ts_data, gen_cat == "Other")
 other$other = 1
-ts_data <- rbind(theft, conduct, kidnapping, controlled, assault, other)
+ts_data <- rbind(theft, vandalism, kidnapping, controlled, assault, other)
 
-ts <- sqldf("SELECT Date, SUM(theft), SUM(conduct), SUM(kidnapping), SUM(controlled), SUM(assault), SUM(other)
+ts <- sqldf("SELECT Date, SUM(theft), SUM(vandalism), SUM(kidnapping), SUM(controlled), SUM(assault), SUM(other)
             FROM ts_data GROUP BY Date")
-names(ts) <- c("Date", "theft", "conduct", "kidnapping", "controlled", "assault", "other")
-write.csv(ts, "sfpd_incidents_2014_ts.csv", row.names = FALSE)
+names(ts) <- c("Date", "Theft", "Vandalism", "Kidnapping", "Controlled", "Assault", "Other")
+write.csv(ts, "sfpd_incidents_2014_ts_crime.csv", row.names = FALSE)
+
+ts_data <- mutate(data, southern = 0)
+ts_data <- mutate(ts_data, mission = 0)
+ts_data <- mutate(ts_data, northern = 0)
+ts_data <- mutate(ts_data, central = 0)
+ts_data <- mutate(ts_data, bayview = 0)
+ts_data <- mutate(ts_data, ingleside = 0)
+ts_data <- mutate(ts_data, tenderloin = 0)
+ts_data <- mutate(ts_data, taraval = 0)
+ts_data <- mutate(ts_data, park = 0)
+ts_data <- mutate(ts_data, richmond = 0)
+
+southern <- filter(ts_data, PdDistrict == "Southern")
+southern$southern = 1
+mission <- filter(ts_data, PdDistrict == "Mission")
+mission$mission = 1
+northern <- filter(ts_data, PdDistrict == "Northern")
+northern$northern = 1
+central <- filter(ts_data, PdDistrict == "Central")
+central$central = 1
+bayview <- filter(ts_data, PdDistrict == "Bayview")
+bayview$bayview = 1
+ingleside <- filter(ts_data, PdDistrict == "Ingleside")
+ingleside$ingleside = 1
+tenderloin <- filter(ts_data, PdDistrict == "Tenderloin")
+tenderloin$tenderloin = 1
+taraval <- filter(ts_data, PdDistrict == "Taraval")
+taraval$taraval = 1
+park <- filter(ts_data, PdDistrict == "Park")
+park$park = 1
+richmond <- filter(ts_data, PdDistrict == "Richmond")
+richmond$richmond = 1
+
+ts_data <- rbind(southern, mission, northern, central, bayview, ingleside, tenderloin, taraval, park, richmond)
+
+ts <- sqldf('SELECT Date, SUM(southern), SUM(mission), SUM(northern), SUM(central), SUM(bayview), SUM(ingleside),
+            SUM(tenderloin), SUM(taraval), SUM(park), SUM(richmond) FROM ts_data GROUP BY Date')
+
+names(ts) <- c('Date', 'Southern', 'Mission', 'Northern', 'Central', 'Bayview', 'Ingleside',
+               'Tenderloin', 'Taraval', 'Park', 'Richmond')
+
+write.csv(ts, 'sfpd_incidents_2014_ts_neighborhoods.csv', row.names = FALSE)
